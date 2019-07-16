ebc72e87e7502ee66604ecd37543d3a20b5d6647
auroradevelopers9 <auroradevelopment9@gmail.com>
2019-07-17 02:27:11 +0300

    ShardsAgensy

---
+++ Shards Agensy/js/index.js
@@ -0,0 +1,7 @@
+$( () => {
+	$(window).scroll( () => {
+		var windowTop = $(window).scrollTop();
+		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+	});
+});
