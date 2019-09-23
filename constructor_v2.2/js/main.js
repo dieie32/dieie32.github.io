3d42d87e44703b6b7ea9024adeef8081e8e3ba37
auroradevelopers9 <auroradevelopment9@gmail.com>
2019-09-23 21:35:27 +0300

    Constructor_v2.2

---
+++ constructor_v2.2/js/main.js
@@ -0,0 +1,522 @@
+
+function calc() {
+
+
+var radOne = document.getElementById('radOne');
+var radTwo = document.getElementById('radTwo');
+
+if (radOne.checked) {
+
+
+if ( document.getElementById('secectorOne').value == 1070 ) {
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 0 ) {
+  		var selectorResultOne = 55;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 1 ) {
+  		var selectorResultOne = 90;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 2 ) {
+  		var selectorResultOne = 150;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 3 ) {
+  		var selectorResultOne = 220;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 4 ) {
+  		var selectorResultOne = 210;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 5 ) {
+  		var selectorResultOne = 175;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 6 ) {
+  		var selectorResultOne = 1050;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1120 ) {
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 0 ) {
+  		var selectorResultOne = 60;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 1 ) {
+  		var selectorResultOne = 100;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 2 ) {
+  		var selectorResultOne = 170;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 3 ) {
+  		var selectorResultOne = 240;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 4 ) {
+  		var selectorResultOne = 235;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 5 ) {
+  		var selectorResultOne = 190;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 6 ) {
+  		var selectorResultOne = 1180;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1310 ) {
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 0 ) {
+  		var selectorResultOne = 80;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 1 ) {
+  		var selectorResultOne = 140;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 2 ) {
+  		var selectorResultOne = 230;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 3 ) {
+  		var selectorResultOne = 330;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 4 ) {
+  		var selectorResultOne = 315;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 5 ) {
+  		var selectorResultOne = 250;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 6 ) {
+  		var selectorResultOne = 1580;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1395 ) {
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 0 ) {
+  		var selectorResultOne = 95;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 1 ) {
+  		var selectorResultOne = 160;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 2 ) {
+  		var selectorResultOne = 270;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 3 ) {
+  		var selectorResultOne = 380;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 4 ) {
+  		var selectorResultOne = 365;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 5 ) {
+  		var selectorResultOne = 290;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 6 ) {
+  		var selectorResultOne = 1840;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1495 ) {
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 0 ) {
+  		var selectorResultOne = 110;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 1 ) {
+  		var selectorResultOne = 180;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 2 ) {
+  		var selectorResultOne = 300;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 3 ) {
+  		var selectorResultOne = 440;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 4 ) {
+  		var selectorResultOne = 415;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 5 ) {
+  		var selectorResultOne = 340;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 6 ) {
+  		var selectorResultOne = 2110;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1595 ) {
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 0 ) {
+  		var selectorResultOne = 125;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 1 ) {
+  		var selectorResultOne = 200;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 2 ) {
+  		var selectorResultOne = 340;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 3 ) {
+  		var selectorResultOne = 490;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 4 ) {
+  		var selectorResultOne = 470;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 5 ) {
+  		var selectorResultOne = 380;
+  }
+
+  if ( document.getElementById('secectorLayerOne').selectedIndex == 6 ) {
+  		var selectorResultOne = 2370;
+  }
+}
+
+
+
+
+if ( document.getElementById('secectorOne').value == 1070 ) {
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 0 ) {
+  		var selectorResultTwo = 1620;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 1 ) {
+  		var selectorResultTwo = 6555;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 2 ) {
+  		var selectorResultTwo = 1560;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1120 ) {
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 0 ) {
+  		var selectorResultTwo = 1820;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 1 ) {
+  		var selectorResultTwo = 6960;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 2 ) {
+  		var selectorResultTwo = 1755;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1310 ) {
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 0 ) {
+  		var selectorResultTwo = 2400;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 1 ) {
+  		var selectorResultTwo = 9280;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 2 ) {
+  		var selectorResultTwo = 2340;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1395 ) {
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 0 ) {
+  		var selectorResultTwo = 2790;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 1 ) {
+  		var selectorResultTwo = 10830;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 2 ) {
+  		var selectorResultTwo = 2730;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1495 ) {
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 0 ) {
+  		var selectorResultTwo = 3180;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 1 ) {
+  		var selectorResultTwo = 14110;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 2 ) {
+  		var selectorResultTwo = 3120;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1595 ) {
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 0 ) {
+  		var selectorResultTwo = 3570;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 1 ) {
+  		var selectorResultTwo = 14805;
+  }
+
+  if ( document.getElementById('secectorLayerTwo').selectedIndex == 2 ) {
+  		var selectorResultTwo = 3510;
+  }
+}
+
+
+
+
+if ( document.getElementById('secectorOne').value == 1070 ) {
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 0 ) {
+      var selectorResultThree = 55;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 1 ) {
+      var selectorResultThree = 90;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 2 ) {
+      var selectorResultThree = 150;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 3 ) {
+      var selectorResultThree = 220;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 4 ) {
+      var selectorResultThree = 210;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 5 ) {
+      var selectorResultThree = 175;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 6 ) {
+      var selectorResultThree = 1050;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1120 ) {
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 0 ) {
+      var selectorResultThree = 60;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 1 ) {
+      var selectorResultThree = 100;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 2 ) {
+      var selectorResultThree = 170;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 3 ) {
+      var selectorResultThree = 240;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 4 ) {
+      var selectorResultThree = 235;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 5 ) {
+      var selectorResultThree = 190;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 6 ) {
+      var selectorResultThree = 1180;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1310 ) {
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 0 ) {
+      var selectorResultThree = 80;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 1 ) {
+      var selectorResultThree = 140;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 2 ) {
+      var selectorResultThree = 230;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 3 ) {
+      var selectorResultThree = 330;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 4 ) {
+      var selectorResultThree = 315;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 5 ) {
+      var selectorResultThree = 250;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 6 ) {
+      var selectorResultThree = 1580;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1395 ) {
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 0 ) {
+      var selectorResultThree = 95;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 1 ) {
+      var selectorResultThree = 160;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 2 ) {
+      var selectorResultThree = 270;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 3 ) {
+      var selectorResultThree = 380;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 4 ) {
+      var selectorResultThree = 365;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 5 ) {
+      var selectorResultThree = 290;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 6 ) {
+      var selectorResultThree = 1840;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1495 ) {
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 0 ) {
+      var selectorResultThree = 110;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 1 ) {
+      var selectorResultThree = 180;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 2 ) {
+      var selectorResultThree = 300;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 3 ) {
+      var selectorResultThree = 440;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 4 ) {
+      var selectorResultThree = 415;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 5 ) {
+      var selectorResultThree = 340;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 6 ) {
+      var selectorResultThree = 2110;
+  }
+}
+if ( document.getElementById('secectorOne').value == 1595 ) {
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 0 ) {
+      var selectorResultThree = 125;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 1 ) {
+      var selectorResultThree = 200;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 2 ) {
+      var selectorResultThree = 340;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 3 ) {
+      var selectorResultThree = 490;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 4 ) {
+      var selectorResultThree = 470;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 5 ) {
+      var selectorResultThree = 380;
+  }
+
+  if ( document.getElementById('secectorLayerThree').selectedIndex == 6 ) {
+      var selectorResultThree = 2370;
+  }
+}
+
+
+
+result = 
+  selectorResultOne + 
+  selectorResultTwo +
+  selectorResultThree +
+  Number(document.getElementById('secectorOne').value);
+
+
+cbxTwo = document.getElementById('cbxTwo');
+cbxThree = document.getElementById('cbxThree');
+
+if (cbxTwo.checked) {
+	result = result + 40;
+}
+
+if (cbxThree.checked) {
+	result = result + 20;
+}
+
+}
+
+
+  document.getElementById('result').value = result;
+}
+
+if (radTwo.checked) { }
+
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
