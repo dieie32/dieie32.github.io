c6e8165d307f3a34cb194e8e626768a1ece4b5c3
dieie32 <46137635+dieie32@users.noreply.github.com>
2020-02-11 17:49:47 +0200

    update gs

--- GS Agency/assets/js/main.js
+++ GS Agency/assets/js/main.js
@@ -203,6 +203,21 @@
   });
 })(jQuery);
 
+//Page cursors
+
+$(window).mousemove(function(e) {
+  $(".cursor").css({
+    left: e.pageX,
+    top: e.pageY
+  });
+});
+$(".cursor-link")
+  .on("mouseenter", function() {
+    $(".cursor").addClass("active");
+  })
+  .on("mouseleave", function() {
+    $(".cursor").removeClass("active");
+  });
 
 particlesJS("particles-js", {
   particles: {
