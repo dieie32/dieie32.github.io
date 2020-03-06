918e0532f9f9abbf11af16493fd9ec90f85ddf95
dieie32 <46137635+dieie32@users.noreply.github.com>
2020-03-06 15:34:30 +0200

    update portfolio

--- Project Shopy/js/home-adv.js
+++
@@ -1,9 +0,0 @@
-let advWidthContainer = $('.adv-width')[0],
-	advWidth 		  = $('.adv').width();
-
-	advWidthContainer.innerHTML = advWidth;
-
-window.onresize = function() {
-	advWidth 					= $('.adv').width();
-	advWidthContainer.innerHTML = advWidth;
-};
--- Project Shopy/js/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	function include(src) {
-		let getScript = document.createElement('script');
-		getScript.setAttribute("src", src);
-		document.getElementsByTagName('body')[0].appendChild(getScript);
-	}
-});
